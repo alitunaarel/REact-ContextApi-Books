2f67dda5d08e368fb16961f8454b33620cca4882
ali <ali@iMac-de-Elodie.fritz.box>
2020-10-27 23:04:47 +0100

    v-4

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import BookList from './BookList';
+import BookContextProvider from '../contexts/BookContext';
+
+
+class App extends React.Component {
+
+  render() {
+    return <div>
+      <BookContextProvider>
+        <BookList />
+      </BookContextProvider>
+    </div>
+  }
+
+}
+
+
+export default App;
